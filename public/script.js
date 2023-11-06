ca4b29390459d2717255662cbc7f2e1bb24cfcc5
Carolina Turner <carolinanturner@gmail.com>
2023-11-05 20:53:52 -0500

    message

---
+++ public/script.js
@@ -0,0 +1,54 @@
+//Carolina Turner CSCE 242
+const getJSON = async ()=>{
+    try{
+        let response = await fetch("http://localhost:3000/api/singers");
+       return await response.json();
+    }   catch (error){
+    console.log("error retrieving json");
+    return "";
+    }
+}
+
+const showSingers = async () => {
+const singerJSON = await getJSON();
+    if(singerJSON == ""){
+        return ;
+    }
+    let singerDiv= document.getElementById("singers-list");
+
+    singerJSON.forEach((singer)=>{
+        let section = document.createElement("section");
+        singerDiv.append(section);
+        let title=singer.name;
+
+        let h3=document.createElement("h3");
+        section.append(h3);
+        h3.innerHTML = singer.name;
+
+        let img = document.createElement("img");
+        section.append(img);
+        img.src="http://localhost:3000/" + singer.img;
+
+        let ul = document.createElement("ul");
+        section.append(ul);
+        ul.append(getLi(`Band/Solo Artist: ${singer.band}`));
+        ul.append(getLi(`My Favorite Song By ${singer.name}: ${singer.best_song}`));
+        ul.append(getLi(`Years Active: ${singer.active}`));
+        ul.append(getLi(`Birthplace: ${singer.born}`));
+        ul.append(getLi(`Genre(s) ${singer.genre}`));
+        section.classList.add("singer");
+    
+        return section;
+    
+    });
+}
+ 
+const getLi = data=>{
+    const li =document.createElement("li");
+    li.textContent= data;
+    return li;
+}
+
+window.onload = () =>{
+    showSingers();
+};
--- server.js
+++ server.js
@@ -1,22 +1,65 @@
 
 const express = require("express");
-
+//setting up with express method 
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/" , (req,res)=>{
-    res.send("hello yay!");
+    res.sendFile(__dirname+ "/index.html");
 });
 
-app.get("/api/cakes", (req, res) => {
-    const cakes = [
-      "birthday cake",
-      "red velvet cake",
-      "chocolate cake",
-      "chesse cake",
-    ];
-    res.send(cakes);
+app.get("/api/singers", (req, res) => {
+    const singers = [];
+    singers[0]={
+        name :"Paul McCartney",
+        band:"The Beatles",
+        best_song:"Hey Jude",
+        active:"1957-Present",
+        born: "Liverpool, England",
+        genre: "Classic Rock, Psychedelic Rock, Pop",
+        img: "images/paulmccartney.jpg"
+    };
+    singers[1]={
+        name :"Elton John",
+        band:"Solo Artist",
+        best_song:"Yellow Brick Road",
+        active:"1962-Present",
+        born: "Pinner, Middlesex, England",
+        genre: "Classic Rock, Pop",
+        img: "images/eltonjohn.jpg"
+    };
+    singers[2]={
+        name :"Tom DeLonge",
+        band:"Blink-182",
+        best_song:"Misery",
+        active:"1992-Present",
+        born: "Poway, California",
+        genre: "Pop Punk, Alternative Rock",
+        img: "images/blink-182.jpg"
+    };
+    singers[3]={
+        name :"Matt Shultz",
+        band:"Cage The Elephant",
+        best_song:"Japanese Buffalo",
+        active:"2001-Present",
+        born: "Bowling Green, Kentucky",
+        genre: "Indie Rock, Psychedelic Rock, Alternative Rock",
+        img: "images/cagetheelephant.jpg"
+    };
+    singers[4]={
+        name :"Billy Joel",
+        band:"Solo Artist",
+        best_song:"Piano Man",
+        active:"1964-Present",
+        born: "New York City, New York",
+        genre: "Classic Rock, Jazz, Pop",
+        img: "images/billyjoel.jpg"
+    };
+      
+    res.json(singers);
   });
-  
+
 app.listen(3000, ()=>{
     console.log("testing");
 
